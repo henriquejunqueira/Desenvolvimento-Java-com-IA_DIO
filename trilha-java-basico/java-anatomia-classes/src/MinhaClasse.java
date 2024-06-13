3dd275803ac8e50a95386a46d99ea2f28d26f7b7
Henrique Junqueira <henriquejunqueira15@gmail.com>
2024-06-13 18:44:19 -0300

    trilha java básico

---
+++ trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,13 @@
+public class MinhaClasse {
+    
+    public static void main (String [] args){
+
+        int ano = 2024;
+        ano = 2022;
+
+        final String BR = "Brasil";
+        // BR = "Brazil";
+
+    }
+
+}
